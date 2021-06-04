886bce9098e55a4ec4f74c31149af90f23362bd9
tiggrra <78557674+tiggrra@users.noreply.github.com>
2021-06-04 16:45:21 +0200

    added data update on api call

--- src/Weather.js
+++ src/Weather.js
@@ -1,39 +1,37 @@
 import React from "react";
-import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  const apiKey = "01bc9da346c1591ec92736f4f11269b6";
-  let city = "Vienna";
-  let apiUnit = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units={apiUnit}`;
-    
+export default function Weather(props) {
+  if (props.status === "clicked" && props.city !== null) {
       return (
         <div className="Weather">
           <div className="CurrentLocation">
-            <h1>{city}</h1>
+            <h1>{props.city}</h1>
             <hr />
             <h2>
               <div>15:09</div>
-              <div>Mostly Sunny</div>
+              <div>{props.condition}</div>
             </h2>
           </div>
           <div className="row">
-            <div className="col-sm-2 current-temp">27</div>
+            <div className="col-sm-2 current-temp">{props.temperature}</div>
             <div className="col-sm-2 measure-unit">
                 <a href="/">°C</a> | <a href="/">°F</a>
             </div>
             <div className="col-sm-3">
-              <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly-sunny"/>
+              <img src={props.icon} alt="weather-icon" />
             </div>
             <div className="col-sm-5 weather-parameters">
                 <ul>
                   <li>Precipitation: 0%</li>
-                  <li>Humidity: 34%</li>
-                  <li>Wind: 13 km/h</li>
+                  <li>Humidity: {props.humidity}%</li>
+                  <li>Wind: {props.wind} km/h</li>
                 </ul>
             </div>
           </div>
         </div>
       );
-}
+    } else {
+      return <div></div>;
+    }
+  }
